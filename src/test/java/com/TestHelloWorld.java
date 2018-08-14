fb9a5c997b181d45a64ccb73dbe30841d49ac116
yebing <yebing@vvsmark.com>
2018-08-15 00:37:43 +0800

    update

--- src/test/java/com/TestHelloWorld.java
+++ src/test/java/com/TestHelloWorld.java
@@ -1,6 +1,13 @@
 package com;
 
+import java.util.List;
+
+import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +19,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class TestHelloWorld {
 
 	@Autowired
+	private RuntimeService runtimeService;
+
+	@Autowired
 	private RepositoryService repositoryService;
+
+	@Autowired
+	private TaskService taskService;
+
+	@Autowired
+	private HistoryService historyService;
 
 	/**部署流程定义*/
 	@Test
 	public void deployProcessDefinition() {
-		
+		Deployment deploy = repositoryService.createDeployment().addClasspathResource("com/diagrams/process.bpmn20.xml").deploy();
+		ProcessDefinition define = repositoryService.createProcessDefinitionQuery().deploymentId(deploy.getId()).singleResult();
+		List list = historyService.createHistoricProcessInstanceQuery().processDefinitionKey(define.getKey()).list();
+		System.out.println(list.size());
 	}
 }
