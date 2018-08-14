80622759efa411d87ea9d2edaa3856bfb1b82950
yebing <yebing@vvsmark.com>
2018-08-15 01:46:58 +0800

    update

--- src/test/java/com/TestHelloWorld.java
+++ src/test/java/com/TestHelloWorld.java
@@ -1,5 +1,6 @@
 package com;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -9,8 +10,13 @@ import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.activiti.engine.impl.pvm.PvmTransition;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +40,7 @@ public class TestHelloWorld {
 	private HistoryService historyService;
 
 	/**部署流程定义*/
-	@Test
+	// @Test
 	public void deployProcessDefinition() {
 		List<Task> taskList = taskService.createTaskQuery().taskAssignee("kermit").list();
 		System.out.println(taskList.size());
@@ -50,13 +56,55 @@ public class TestHelloWorld {
 		}
 	}
 
-	@Test
 	public void completeTaskTest() {
 		// 任务ID
 		String taskId = "10064";
-		taskService.setAssignee("kermit","kermit");
 		taskService.complete(taskId);
 		System.out.println("完成任务：任务ID：" + taskId);
+	}
+
+	@Test
+	public void queryTask() {
+		//返回存放连线的名称集合  
+        List<String> list = new ArrayList<String>();
+		Task task = taskService.createTaskQuery().taskId("10064").singleResult();
+		System.out.println("====================");
+		System.out.println("任务ID:" + task.getId());
+		System.out.println("任务名称:" + task.getName());
+		System.out.println("任务的创建时间:" + task.getCreateTime());
+		System.out.println("任务的办理人:" + task.getAssignee());
+		System.out.println("流程实例ID：" + task.getProcessInstanceId());
+		System.out.println("执行对象ID:" + task.getExecutionId());
+		System.out.println("流程定义ID:" + task.getProcessDefinitionId());
+
+		// 2:获取流程定义ID
+		String processDefinitionId = task.getProcessDefinitionId();
+		// 3:查询ProcessDefinitionEntity对象
+		ProcessDefinitionEntity processDefinitionEntity = (ProcessDefinitionEntity) repositoryService.getProcessDefinition(processDefinitionId);
+
+		// 获取流程实例ID
+		String processInstanceId = task.getProcessInstanceId();
+		// 获取流程实例
+		ProcessInstance pi = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+		// 获取当前活动ID
+		String activityId = pi.getActivityId();
+
+		// 4：获取当前的活动
+		ActivityImpl activityImpl = processDefinitionEntity.findActivity(activityId);
+		// 5:获取当前活动完成之后连线的名称
+		List<PvmTransition> pvmList = activityImpl.getOutgoingTransitions();
+		if (pvmList != null && pvmList.size() > 0) {
+			for (PvmTransition pvm : pvmList) {
+				String name = (String) pvm.getProperty("name");
+				if (StringUtils.isNotBlank(name)) {
+					list.add(name);
+					System.out.println(name);
+				} else {
+					list.add("默认提交");
+					System.out.println("默认提交");
+				}
+			}
+		}
 	}
 
 	public void test1() {
